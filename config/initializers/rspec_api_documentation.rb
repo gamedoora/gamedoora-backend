e8a4182154eca4b0ef46aede1d126d0baf95a193
simran-sawhney <simran2194@gmail.com>
2019-12-10 11:52:45 +0530

    Rspec doc fix

--- config/initializers/rspec_api_documentation.rb
+++ config/initializers/rspec_api_documentation.rb
@@ -1,10 +1,12 @@
 # config/initializers/rspec_api_documentation.rb
-RspecApiDocumentation.configure do |config|
-  # Output folder
-  # config.docs_dir = Rails.root.join("doc", "api")
+if defined? (RspecApiDocumentation)
+  RspecApiDocumentation.configure do |config|
+    # Output folder
+    # config.docs_dir = Rails.root.join("doc", "api")
 
-  # An array of output format(s).
-  # Possible values are :json, :html, :combined_text, :combined_json,
-  #   :json_iodocs, :textile, :markdown, :append_json
-  config.format = [:json]
+    # An array of output format(s).
+    # Possible values are :json, :html, :combined_text, :combined_json,
+    #   :json_iodocs, :textile, :markdown, :append_json
+    config.format = [:json]
+  end
 end
